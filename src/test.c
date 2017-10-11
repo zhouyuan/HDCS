cad3af3c81b78f0965cfc446a8c6007c1ed4c797
Chendi.Xue <chendi.xue@intel.com>
2017-10-11 16:03:15 +0800

    Add a flush/promote tool for HDCS
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- src/test.c
+++ src/test.c
@@ -1,5 +1,6 @@
-#include <hdcs/libhdcs.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "include/libhdcs.h"
 
 struct hdcs_data {
   hdcs_ioctx_t io;
@@ -22,13 +23,21 @@ static void hdcs_disconnect(struct hdcs_data *hdcs)
 }
 
 int main(int argc, char **argv) {
+  if (argc < 3) {
+    printf("usage:\n\t %s flush/promote ${hdcs_instance_name}\n\ttips: Make sure there is a general.conf under execution dir.\n\n", argv[0]);
+    return 1;
+  }
 	struct hdcs_data *hdcs;
 	hdcs = (struct hdcs_data*)calloc(1, sizeof(struct hdcs_data));
 	if (!hdcs)
 		goto failed;
 
-  hdcs_connect(hdcs, argv[1]);
-  hdcs_promote_all(hdcs->io);
+  hdcs_connect(hdcs, argv[2]);
+  if( argv[1][0] == 'p' ) {
+    hdcs_promote_all(hdcs->io);
+  } else {
+    hdcs_flush_all(hdcs->io);
+  }
   hdcs_disconnect(hdcs);
   return 0;
 
